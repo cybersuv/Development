00686f4dae8dacd8281802fc478b796f74a4ec3a
esuvbha <esuvbha@12CL882.ericsson.se>
2017-11-20 17:33:41 +0530

    Azure logging working initially. But, need to check with application specific log messages.

--- AzureJavaSDKTest/src/main/java/com/suvankar/development/azure/poc/App.java
+++ AzureJavaSDKTest/src/main/java/com/suvankar/development/azure/poc/App.java
@@ -1,5 +1,7 @@
 package com.suvankar.development.azure.poc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -7,16 +9,18 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@ComponentScan(basePackages={"com.suvankar.development.azure.poc.*"})
+@ComponentScan(basePackages = { "com.suvankar.development.azure.poc.*" })
 @SpringBootApplication
-public class App  extends SpringBootServletInitializer
-{
+public class App extends SpringBootServletInitializer {
+	static Logger logger = LoggerFactory.getLogger("defaultLogger");
+	
 	@Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-      return application.sources(App.class);
-  }
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
 
-  public static void main(String[] args) throws Exception {
-      SpringApplication.run(App.class, args);
-  }
+	public static void main(String[] args) throws Exception {
+		logger.debug("########## Booting up application ##########");
+		SpringApplication.run(App.class, args);
+	}
 }
--- AzureJavaSDKTest/src/main/java/com/suvankar/development/azure/poc/controller/WelcomeController.java
+++ AzureJavaSDKTest/src/main/java/com/suvankar/development/azure/poc/controller/WelcomeController.java
@@ -1,12 +1,17 @@
 package com.suvankar.development.azure.poc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+	static Logger logger = LoggerFactory.getLogger("defaultLogger");
 	@RequestMapping("/")
 	public String welcome() {
+		logger.debug("Got request at root context path.");
+		logger.debug("Returning response..");
 		return "Response from Spring-Boot WelcomeController !!";
 	}
 }
